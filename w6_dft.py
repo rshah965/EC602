331134f992f5cb3f38d38f4ad8c39babbca59eb4
Rishab Shah <rishah@bu.edu>
2016-10-15 18:32:28 -0400

    v5 - dict removed as valid input

--- w6_dft.py
+++ w6_dft.py
@@ -3,9 +3,7 @@
 from numpy import zeros,exp,array,pi
 
 def DFT(x):
-	if (type(x) is dict):
-		x=(x.values())
-	if (type(x) is complex or type(x) is int or type(x) is float):
+	if (type(x) is complex or type(x) is int or type(x) is float or type(x) is dict):
 		raise ValueError('Input should be a sequence of values only.')
 	for i in range(len(x)):
 		if (type(x[i]) is tuple or type(x[i]) is list or type(x[i]) is dict or type(x[i]) is str):
