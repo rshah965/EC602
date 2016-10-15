2ed6b73e9fc6595ca4b83b5d289a18f71368056e
Rishab Shah <rishah@bu.edu>
2016-10-15 19:54:28 -0400

    v6

--- w6_dft.py
+++ w6_dft.py
@@ -6,7 +6,7 @@ def DFT(x):
 	if (type(x) is complex or type(x) is int or type(x) is float or type(x) is dict):
 		raise ValueError('Input should be a sequence of values only.')
 	for i in range(len(x)):
-		if (type(x[i]) is tuple or type(x[i]) is list or type(x[i]) is dict or type(x[i]) is str):
+		if (type(x[i]) is tuple or type(x[i]) is list or type(x[i]) is dict or type(x[i]) is str or type(x[i]) is numpy.ndarray):
 			raise ValueError('Input should be a sequence of values only.')
 	X=zeros((len(x),))
 	N=len(x)
