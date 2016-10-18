3a98d41ba66286a0de92acd646706d6830efd46b
Rishab Shah <rishah@bu.edu>
2016-10-17 20:35:49 -0400

    Add files via upload

--- w6_dft.py
+++ w6_dft.py
@@ -3,14 +3,17 @@
 from numpy import zeros,exp,array,pi
 
 def DFT(x):
-	if (type(x) is dict or type(x) is str):
-		raise ValueError('Input should be a sequence of values only.')
-	for i in range(len(x)):
-		if (type(x[i]) is tuple or type(x[i]) is list or type(x[i]) is dict or type(x[i]) is str or type(x[i]) is numpy.ndarray):
-			raise ValueError('Input should be a sequence of values only.')
-	X=zeros((len(x),))
-	N=len(x)
-	n=array(range(N))
-	k=n[:,None]
-	E=exp(-2j * pi * k * n / N)
+	try:
+		'''if (type(x) is dict or type(x) is str or type(x) is set or type(x) is frozenset or type(x) is function):
+			raise ValueError('Input should be a sequence of values only.')'''
+		for i in range(len(x)):
+			if (type(x[i]) is tuple or type(x[i]) is list or type(x[i]) is dict or type(x[i]) is str):
+				raise ValueError('Input should be a sequence of values only.')
+		X=zeros((len(x),))
+		N=len(x)
+		n=array(range(N))
+		k=n[:,None]
+		E=exp(-2j * pi * k * n / N)
+	except:
+		raise ValueError
 	return sum(a*b for a,b in zip(E,x))
