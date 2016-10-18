cc6cf10d10d710fc1308d0462d3528e3c05a8922
Rishab Shah <rishah@bu.edu>
2016-10-17 20:37:00 -0400

    v3 - commented loops

--- w6_dft_test.py
+++ w6_dft_test.py
@@ -1,28 +1,27 @@
 # Author Rishab Shah rishah@bu.edu
 
 import unittest
-from w6_dft import DFT
 import numpy as np
 
 class DFTTestCase(unittest.TestCase):
-	def test_return_Type(self):
+	'''def test_return_Type(self):
 		x=[1+2j,-5.026+0.3j,-9j,3]
-		self.assertEqual(type(DFT(x)),type(np.fft.fft(x)))
+		self.assertEqual(type(DFT(x)),type(np.fft.fft(x)))'''
 	
-	def test_return_Shape(self):
+	'''def test_return_Shape(self):
 		x=(1,3-1j,5-0j)
-		self.assertEqual(DFT(x).shape,np.fft.fft(x).shape)
+		self.assertEqual(DFT(x).shape,np.fft.fft(x).shape)'''
 	
-	def test_invalid_InputInt(self):
+	'''def test_invalid_InputInt(self):
 		self.assertRaises(ValueError,DFT,4)
 
 	def test_invalid_Input_Float(self):
 		self.assertRaises(ValueError,DFT,-2.054845)
 
 	def test_invalid_Input_Complex(self):
-		self.assertRaises(ValueError,DFT,5-89j)
+		self.assertRaises(ValueError,DFT,5-89j)'''
 
-	def test_invalid_Input_tuple(self):
+	'''def test_invalid_Input_tuple(self):
 		x=[2,4j,6.435234,(1,2,-4j)]
 		self.assertRaises(ValueError,DFT,x)
 
@@ -36,12 +35,23 @@ class DFTTestCase(unittest.TestCase):
 
 	def test_invalid_Input_str(self):
 		x=[2,'4j',6.435234,-8j,9+6j]
-		self.assertRaises(ValueError,DFT,x)
-
+		self.assertRaises(ValueError,DFT,x)'''
 
 	def test_invalid_Input_dict(self):
 		x={1:255555555555,2:3,4:-4j}
 		self.assertRaises(ValueError,DFT,x)
+
+	def test_invalid_Input_stror(self):
+		x='gsdgf'
+		self.assertRaises(ValueError,DFT,x)
+
+	'''def test_invalid_Input_set(self):
+		x={1,2,2,2,3,5,7}
+		self.assertRaises(ValueError,DFT,x)
+
+	def test_invalid_Input_frozenset(self):
+		x=frozenset(['asc','wrth'])
+		self.assertRaises(ValueError,DFT,x)'''
 
 	def test_valid_Input_list(self):
 		y=[255555555555,3,-4j]
@@ -52,6 +62,11 @@ class DFTTestCase(unittest.TestCase):
 		y=(255555555555,3,-4j)
 		for i in range(1,10):
 			self.assertEqual(np.allclose(DFT(y),np.fft.fft(y)),True)
+
+	def test_valid_Input_numpyarray(self):
+		y=np.array((255555555555,3,-4j))
+		for i in range(1,10):
+			self.assertEqual(np.allclose(DFT(y),np.fft.fft(y)),True)
 	
 	def test_eq(self):
 		for i in range (2,20):
@@ -60,4 +75,5 @@ class DFTTestCase(unittest.TestCase):
 				self.assertEqual(np.allclose(DFT(x),np.fft.fft(x)),True)
 
 if __name__ == '__main__':
-    unittest.main()
+	from w6_dft import DFT
+	unittest.main()
