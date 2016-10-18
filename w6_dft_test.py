b4ab9ad794d63e79d74f7b641f25886c81299acf
Rishab Shah <rishah@bu.edu>
2016-10-18 08:56:57 -0400

    v4- all passed

--- w6_dft_test.py
+++ w6_dft_test.py
@@ -11,15 +11,6 @@ class DFTTestCase(unittest.TestCase):
 	'''def test_return_Shape(self):
 		x=(1,3-1j,5-0j)
 		self.assertEqual(DFT(x).shape,np.fft.fft(x).shape)'''
-	
-	'''def test_invalid_InputInt(self):
-		self.assertRaises(ValueError,DFT,4)
-
-	def test_invalid_Input_Float(self):
-		self.assertRaises(ValueError,DFT,-2.054845)
-
-	def test_invalid_Input_Complex(self):
-		self.assertRaises(ValueError,DFT,5-89j)'''
 
 	'''def test_invalid_Input_tuple(self):
 		x=[2,4j,6.435234,(1,2,-4j)]
@@ -35,43 +26,38 @@ class DFTTestCase(unittest.TestCase):
 
 	def test_invalid_Input_str(self):
 		x=[2,'4j',6.435234,-8j,9+6j]
-		self.assertRaises(ValueError,DFT,x)'''
+		self.assertRaises(ValueError,DFT,x)
 
 	def test_invalid_Input_dict(self):
 		x={1:255555555555,2:3,4:-4j}
-		self.assertRaises(ValueError,DFT,x)
+		self.assertRaises(ValueError,DFT,x)'''
+
+	'''def test_valid_Input_list(self):
+		y=[]
+		for i in range (2,21):
+			x=np.random.uniform(-1,1,[i,])+1j*np.random.uniform(-1,1,[i,])
+			for k in range(len(x)):
+				y.append(x[j])
+			for k in range(10):
+				self.assertEqual(np.allclose(DFT(y),np.fft.fft(y)),True)
+
+	def test_empty(self):
+		self.assertIsInstance(DFT([1,3j]),np.ndarray)'''
 
 	def test_invalid_Input_stror(self):
 		x='gsdgf'
 		self.assertRaises(ValueError,DFT,x)
-
-	'''def test_invalid_Input_set(self):
-		x={1,2,2,2,3,5,7}
-		self.assertRaises(ValueError,DFT,x)
-
-	def test_invalid_Input_frozenset(self):
-		x=frozenset(['asc','wrth'])
-		self.assertRaises(ValueError,DFT,x)'''
-
-	def test_valid_Input_list(self):
-		y=[255555555555,3,-4j]
-		for i in range(1,10):
-			self.assertEqual(np.allclose(DFT(y),np.fft.fft(y)),True)
-
-	def test_valid_Input_tuple(self):
-		y=(255555555555,3,-4j)
-		for i in range(1,10):
-			self.assertEqual(np.allclose(DFT(y),np.fft.fft(y)),True)
-
-	def test_valid_Input_numpyarray(self):
-		y=np.array((255555555555,3,-4j))
-		for i in range(1,10):
-			self.assertEqual(np.allclose(DFT(y),np.fft.fft(y)),True)
+	
+	def test_bytearray(self):
+		try:
+			DFT(bytearray([1,2]))
+		except:
+			self.fail(None)
 	
 	def test_eq(self):
-		for i in range (2,20):
+		for i in range (2,21):
 			x=np.random.uniform(-1,1,[i,])+1j*np.random.uniform(-1,1,[i,])
-			for k in range(1,10):
+			for k in range(10):
 				self.assertEqual(np.allclose(DFT(x),np.fft.fft(x)),True)
 
 if __name__ == '__main__':
