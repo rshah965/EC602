5a5f34fbddf09984f166b195a46115e51855526c
Rishab Shah <rishah@bu.edu>
2016-11-08 19:39:10 -0500

    v4

--- w8c_multiply.cpp
+++ w8c_multiply.cpp
@@ -1,4 +1,5 @@
-// Author Rishab Shah rishah@bu.edu
+// AUTHOR Rishab Shah rishah@bu.edu
+
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,214 +7,258 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[])
+
+typedef vector< vector<int> > int_m;
+typedef vector< vector<double> > double_m;
+
+int_m mul(int_m A, int_m B){
+    
+    int M = A.size();
+    int N = A[0].size();
+    int L = B[0].size();
+    int sum = 0;
+    
+    int_m c(M);
+    
+    for (int i=0;i<M;i++)
+    for (int j=0;j<L;j++)
+    for (int n=0;n<N;n++){
+        sum = sum + (A[i][n]*B[n][j]);
+        if (n == (N-1)){
+            c[i].push_back(sum);
+            sum = 0;
+        }
+    }
+    return c;
+    
+}
+
+double_m double_mul(double_m A, double_m B){
+    
+    int M = A.size();
+    int N = A[0].size();
+    int L = B[0].size();
+    double sum = 0;
+    
+    double_m c(M);
+    
+    for (int i=0;i<M;i++)
+    for (int j=0;j<L;j++)
+    for (int n=0;n<N;n++){
+        sum = sum + (A[i][n]*B[n][j]);
+        if (n == (N-1)){
+            c[i].push_back(sum);
+            sum = 0;
+        }
+    }
+    return c;
+    
+}
+
+
+
+int main (int argc, char const *argv[])
 {
-    int M,N,L;
-    string s3,s4,s5;
-    ifstream thisfile;
-    double one,two;
-    //string s1(argv[0]);
-    string s2(argv[1]);
-    if (argc ==6 || argc == 8){
-    if (argc==6){
-        M=stoi(argv[2]);
-        N=M;
-        L=M;
-        s3=string(argv[3]);
-        s4=string(argv[4]);
-        s5=string(argv[5]);
-        }
-    else if (argc==8){
-        M=stoi(argv[2]);
-        N=stoi(argv[3]);
-        L=stoi(argv[4]);
-        s3=string(argv[5]);
-        s4=string(argv[6]);
-        s5=string(argv[7]);
+    int M, N, L, i;
+    string s1,s2,s3,s4,s5,s6,s7;
+    if(argc!=8 && argc !=6 ){
+        return 1;
     }
+    if(argc == 8){
+        s1 = argv[1];
+        
+        s2 = argv[2];
+        s3 = argv[3];
+        s4 = argv[4];
+        
+        s5 = argv[5];
+        s6 = argv[6];
+        s7 = argv[7];
+    }
+    
+    if(argc == 6){
+        s1 = argv[1];
+        
+        s2 = argv[2];
+        s3 = argv[2];
+        s4 = argv[2];
+        
+        s5 = argv[3];
+        s6 = argv[4];
+        s7 = argv[5];
+    }
+    
+    M = atoi(s2.c_str());
+    N = atoi(s3.c_str());
+    L = atoi(s4.c_str());
+    //cout<<M<<" "<<N<<" "<<L<<endl;
+    if(s1!="int" && s1!="double" ){
+        return 1;
+    }
+    
+    if(M<=0 || N<=0 || L<=0 ){
+        return 1;
+    }
+    
+    //cout<<d<<endl;
+    
+    if(s1== string("int")){
+        vector<vector <int> > A(M);
+        vector<vector <int> > B(N);
+        ifstream thisfile; // note: this is an IFSTREAM, "I" stands for INPUT
+        
+        int one;
+        
+        
+        thisfile.open(s5);
+        if (thisfile.fail()==1){
+            return 2;
+        }
+        i = 0;
+        
+        while (thisfile >> one)
+        {
+            i++;
+        }
+        
+        thisfile.close();
+        if (i != (M*N)){
+            return 3;
+        }
+        thisfile.open(s6);
+        if (thisfile.fail()==1){
+            return 2;
+        }
+        i = 0;
+        
+        while (thisfile >> one)
+        {
+            i++;
+        }
+        
+        thisfile.close();
+        if (i != (N*L)){
+            return 3;
+        }
+        
+        thisfile.open(s5);
+        
+        for (i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                thisfile >> one;
+                A[i].push_back(one);
+            }
+        }
+        thisfile.close();
+        
+        thisfile.open(s6);
+        
+        for (i = 0; i < N; i++){
+            for (int j = 0; j < L; j++){
+                thisfile >> one;
+                B[i].push_back(one);
+            }
+        }
+        
+        thisfile.close();
+        
+        int_m C = mul(A, B);
+        
+        ofstream filewithnumbers; // note: this is an OFSTREAM, "O" stands for OUTPUT
+        
+        filewithnumbers.open(s7);
+        if(filewithnumbers.is_open() == 0){
+            return 4;
+        }
+        for (i=0; i < M; i++){
+            for(int j = 0; j < L; j++){
+                filewithnumbers <<C[i][j]<<" ";
+                if (j == L - 1){
+                    filewithnumbers << endl;
+                }
+            }
+        }
+        
+        filewithnumbers.close();
     }
     else{
-        cout << "213" << endl;
-        return 1;}
-    if (M<=0 || N<=0 || L<=0){
-        return 1;}
-    ofstream thisfile2;
-    thisfile2.open(s5);
-    if (thisfile2.is_open()==0){
-        cout<<"123123"<<endl;
-        return 4;}
-    thisfile2.close();
-    //cout << M << N << L << s3 << s4 << s5 <<endl;
-    if (s2 != "int" && s2 != "double"){
-        //cout << "going in this loop" <<endl;
-        return 1;}
-    if (s2=="int"){
-    vector<vector<int> > A(M, vector<int>(N));
-    vector<vector<int> > B(N, vector<int>(L));
-    ifstream f(s3);
-    if (f.good() == true){
-        //cout << "nice" << endl;
-        thisfile.open(s3);
-        int cnt = 0;
-        while(thisfile >> one){
-            cnt=cnt+1;
+        vector<vector <double> > A(M);
+        vector<vector <double> > B(N);
+        ifstream thisfile; // note: this is an IFSTREAM, "I" stands for INPUT
+        
+        double one;
+        
+        
+        thisfile.open(s5);
+        if (thisfile.fail()==1){
+            return 2;
         }
+        i = 0;
+        
+        while (thisfile >> one)
+        {
+            i++;
+        }
+        
         thisfile.close();
-        thisfile.open(s3);
-        if (cnt == M*N){
-            //cout << "going in loop" << M << N << endl;
-            while (thisfile >> two){
-                for (int i=0; i<M; ++i){
-                    for (int j=0;j<N;++j){
-                        A[i][j]=two;
-                        thisfile >> two;
-                        //cout << "i " << i << " j " << j << " val " << A[i][j] << endl;
-                    }
+        if (i != (M*N)){
+            return 3;
+        }
+        thisfile.open(s6);
+        if (thisfile.fail()==1){
+            return 2;
+        }
+        i = 0;
+        
+        while (thisfile >> one)
+        { 
+            i++;
+        }
+        
+        thisfile.close();
+        if (i != (N*L)){
+            return 3;
+        }
+        
+        thisfile.open(s5);
+        
+        for (i = 0; i < M; i++){
+            for (int j = 0; j < N; j++){
+                thisfile >> one;
+                A[i].push_back(one);
+            }
+        }
+        thisfile.close(); 
+        
+        thisfile.open(s6);
+        
+        for (i = 0; i < N; i++){
+            for (int j = 0; j < L; j++){
+                thisfile >> one;
+                B[i].push_back(one);
+            }
+        }
+        
+        thisfile.close();
+        
+        double_m C = double_mul(A, B);
+        
+        ofstream filewithnumbers; // note: this is an OFSTREAM, "O" stands for OUTPUT
+        
+        filewithnumbers.open(s7);
+        if(filewithnumbers.is_open() == 0){
+            return 4;
+        }
+        for (i=0; i < M; i++){
+            for(int j = 0; j < L; j++){
+                filewithnumbers <<C[i][j]<<" ";
+                if (j == L - 1){
+                    filewithnumbers << endl;
                 }
             }
         }
-        else{
-            return 3;}
-            //cout << "wrong index" << endl;}
-        thisfile.close();}
-    else {
-        return 2;}
-        //cout << "file does not exist" << endl;}
-
-
-    ifstream f2(s4);
-    if (f2.good() == true){
-        //cout << "nice" << endl;
-        thisfile.open(s4);
-        if(thisfile.good() == true){}
-        int cnt = 0;
-        while(thisfile >> one){
-            cnt=cnt+1;
-        }
-        thisfile.close();
-        thisfile.open(s4);
-        if(thisfile.good() == true){}
-        if (cnt == N*L){
-            //cout << "going in loop" << N << L << endl;
-            while (thisfile >> two)
-            {
-                for (int i=0; i<N; ++i){
-                    for (int j=0;j<L;++j){
-                        B[i][j]=two;
-                        thisfile >> two;
-                        //cout << "i " << i << " j " << j << " val " << B[i][j] << endl;
-                    }
-                }
-            }
-        }
-        else{
-            return 3;}
-            //cout << "wrong index" << endl;}
-        thisfile.close();}
-    else{
-        return 2;}
-        //cout << "file does not exist" << endl;}
-
-
-        int l= B[0].size();
-        int m=A.size();
-        int n=A[0].size();
-        vector<vector<int> > c(m,vector<int>(l));
-        for (int i=0;i<m;i++)
-            for (int j=0;j<l;j++)
-                for (int k=0;k<n;k++)
-                    c[i][j] += A[i][k] * B[k][j];
-        ofstream xyz;
-        xyz.open(s5);
-        for (int i=0;i<m;i++)
-            for (int j=0;j<l;j++){
-                //cout << c[i][j] << endl;
-                xyz << c[i][j] << endl;}
-        xyz.close();
-    }
-    if (s2=="double"){
-    vector<vector<double> > A(M, vector<double>(N));
-    vector<vector<double> > B(N, vector<double>(L));
-    ifstream f(s3);
-    if (f.good() == true){
-        //cout << "nice" << endl;
-        thisfile.open(s3);
-        int cnt = 0;
-        while(thisfile >> one){
-            cnt=cnt+1;
-        }
-        thisfile.close();
-        thisfile.open(s3);
-        if (cnt == M*N){
-            //cout << "going in loop" << M << N << endl;
-            while (thisfile >> two){
-                for (int i=0; i<M; ++i){
-                    for (int j=0;j<N;++j){
-                        A[i][j]=two;
-                        thisfile >> two;
-                        //cout << "i " << i << " j " << j << " val " << A[i][j] << endl;
-                    }
-                }
-            }
-        }
-        else{
-            return 3;}
-            //cout << "wrong index" << endl;}
-        thisfile.close();}
-    else {
-        return 2;}
-        //cout << "file does not exist" << endl;}
-
-
-    ifstream f2(s4);
-    if (f2.good() == true){
-        //cout << "nice" << endl;
-        thisfile.open(s4);
-        if(thisfile.good() == true){}
-        int cnt = 0;
-        while(thisfile >> one){
-            cnt=cnt+1;
-        }
-        thisfile.close();
-        thisfile.open(s4);
-        if(thisfile.good() == true){}
-        if (cnt == N*L){
-            //cout << "going in loop" << N << L << endl;
-            while (thisfile >> two)
-            {
-                for (int i=0; i<N; ++i){
-                    for (int j=0;j<L;++j){
-                        B[i][j]=two;
-                        thisfile >> two;
-                        //cout << "i " << i << " j " << j << " val " << B[i][j] << endl;
-                    }
-                }
-            }
-        }
-        else{
-            return 3;}
-            //cout << "wrong index" << endl;}
-        thisfile.close();}
-    else{
-        return 2;}
-        //cout << "file does not exist" << endl;}
-
-
-        int l= B[0].size();
-        int m=A.size();
-        int n=A[0].size();
-        vector<vector<double> > c(m,vector<double>(l));
-        for (int i=0;i<m;i++)
-            for (int j=0;j<l;j++)
-                for (int k=0;k<n;k++)
-                    c[i][j] += A[i][k] * B[k][j];
-        ofstream xyz;
-        xyz.open(s5);
-        for (int i=0;i<m;i++)
-            for (int j=0;j<l;j++){
-                //cout << c[i][j] << endl;
-                xyz << c[i][j] << endl;}
-        xyz.close();
+        
+        filewithnumbers.close();
     }
     return 0;
 }
